7a479de9db26480339ca9210dd7713e1fcaf0dd3
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-17 16:35:08 +0100

    dto for create book

---
+++ src/book/dto/create-book.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+
+  @IsString()
+  category: string;
+
+  @IsString()
+  image: string;
+}
