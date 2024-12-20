4df668810484e41dcc3084a0f04c955fe41e9deb
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-20 15:42:10 +0100

    add cors in main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
