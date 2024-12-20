0a4fb82d4e130f1468f09792c52f71a460cb5447
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-20 15:33:18 +0100

    add categories service

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
+import { CategoryService } from '../categories/categories.service';
 import { BookController } from './book.controller';
 import { DynamoDBModule } from '../database/dynamodb.module';
 
 @Module({
-  providers: [BookService],
+  providers: [BookService, CategoryService],
   controllers: [BookController],
   imports: [DynamoDBModule],
 })
