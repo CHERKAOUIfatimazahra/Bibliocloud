af40e8f779052290561c5fd639f06a170c09444a
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-23 09:19:48 +0100

    add validation for unique category names in CreateCategoryDto

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateCategoryDto {
+  // name unique
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  
+  @IsString()
   description?: string;
 }
